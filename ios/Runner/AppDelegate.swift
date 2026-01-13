9c71b41fe04b6a63a8bc7a6997556e4d9445063d
Abdullah Mohamed <abdullahmohamed@Abdullahs-MacBook-Pro-2.local>
2026-01-13 16:21:31 +0200

    fix notification tap on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import workmanager
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,11 @@ import workmanager
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Set notification delegate for iOS 10+ to handle notification taps
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+    
     WorkmanagerPlugin.registerTask(withIdentifier: "be.tramckrijte.workmanager.background.task")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
